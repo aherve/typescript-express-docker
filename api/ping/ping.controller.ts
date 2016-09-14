525857e9312751c0e0f4a10a104b83aad44d7890
Aurélien Hervé <mail@aurelien-herve.com>
2016-09-14 11:56:19 +0200

    pong

--- api/ping/ping.controller.ts
+++ api/ping/ping.controller.ts
@@ -1,7 +1,7 @@
 import * as express from 'express'
 
 export async function ping (req: express.Request, res: express.Response) {
-  res.status(200).send({ping: 'poong'})
+  res.status(200).send({ping: 'pong'})
   await delay(1000)
   console.log('hello. I waited 1000 ms without blocking the thread')
 }
